9ca66203c1e90de213253fe360a9d1e9e8d1228f
IsmailBogac <92868619+IsmailBogac@users.noreply.github.com>
2022-10-17 15:34:39 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n,i,j;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz :");
+        n = input.nextInt();
+        for (i=1;i<n;i*=4){
+            System.out.println(i);
+        } for (j=1;j<n;j*=5){
+            System.out.println(j);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
